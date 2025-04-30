fae0b035e72590e3ccd526cf78b6f9a966a6a318
Chowdhury85 <145401540+Chowdhury85@users.noreply.github.com>
2025-04-30 19:40:43 -0400

    Create RaceHorse.java

---
+++ RaceHorse.java
@@ -0,0 +1,13 @@
+public class RaceHorse extends Horse {
+    private int racesCompeted;
+
+    // Getter method for racesCompeted
+    public int getRacesCompeted() {
+        return racesCompeted;
+    }
+
+    // Setter method for racesCompeted
+    public void setRacesCompeted(int races) {
+        this.racesCompeted = races;
+    }
+}
